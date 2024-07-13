93051d20b7d6e26200e33ea2306eb6ccd9c208ad
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-07-13 21:00:20 +0100

    create

---
+++ shill-wtf/src/app/Create-Campaign/CreateCampaignForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CreateCampaignForm = () => {
+  return (
+    <div className="flex flex-col justify-center">
+
+
+sdsds
+
+
+    </div>
+  )
+}
+
+export default CreateCampaignForm
--- shill-wtf/src/app/Create-Campaign/page.jsx
+++ shill-wtf/src/app/Create-Campaign/page.jsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function CreateToken() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl">Create Campaign</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="flex gap-10 h-[70vh]">
+        <div className="border w-[40vw] justify-center rounded-3xl">
+
+
+        </div>
+        <div className="border w-[40vw] rounded-3xl">Section 2</div>
+      </div>
     </main>
   );
 }

1ec90ffb9abf521becd8f24e6eae5cb920730d31
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-07-13 23:19:57 +0100

    fucked

--- shill-wtf/src/app/Create-Campaign/CreateCampaignForm.jsx
+++ shill-wtf/src/app/Create-Campaign/CreateCampaignForm.jsx
@@ -3,8 +3,10 @@ import Image from 'next/image'
 
 const CreateCampaignForm = () => {
 return (
-    <div className="flex flex-col justify-center bg-red-500">
-        <Image src="/CreateCampaign/DoubleBox.png" alt="Group 51" width={300} height={300} />
+    <div className="flex flex-col justify-center items-center p-5 m-5 h-[480px] w-[800px]">
+        <div className="w-90 h-90" style={{ maxWidth: '90%', maxHeight: '90%' }}>
+            <Image src="/CreateCampaign/DoubleBox.png" alt="Group 51" layout="fill" objectFit="contain" />
+        </div>
     </div>
 )
 }

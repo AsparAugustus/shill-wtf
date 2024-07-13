3ad6f2f377ad870bafb56e435ef64ca826fcf725
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-07-13 23:31:03 +0100

    yo

--- shill-wtf/src/app/Create-Campaign/CreateCampaignForm.jsx
+++ shill-wtf/src/app/Create-Campaign/CreateCampaignForm.jsx
@@ -3,10 +3,8 @@ import Image from 'next/image'
 
 const CreateCampaignForm = () => {
 return (
-    <div className="flex flex-col justify-center items-center p-5 m-5 h-[480px] w-[800px]">
-        <div className="w-90 h-90" style={{ maxWidth: '90%', maxHeight: '90%' }}>
-            <Image src="/CreateCampaign/DoubleBox.png" alt="Group 51" layout="fill" objectFit="contain" />
-        </div>
+    <div className="flex flex-col justify-center items-center p-5 m-5">
+        <img src="/CreateCampaign/DoubleBox.png" alt="Group 51" width="400" height="600" />
     </div>
 )
 }
--- shill-wtf/src/app/Create-Campaign/page.jsx
+++ shill-wtf/src/app/Create-Campaign/page.jsx
@@ -7,7 +7,7 @@ export default function CreateToken() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24 
     bg-gradient-to-b from-white via-pink-200 to-pink-300">
       <div className="flex gap-10 h-[70vh]">
-        <div className="border border-black h-[480px] w-[800px] justify-center rounded-3xl">
+        <div className="border border-black h-[380px] w-[800px] justify-center rounded-3xl">
 
 
        

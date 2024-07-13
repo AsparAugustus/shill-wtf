3dc28c39261380bd9425d2d8c589eeb6dc2612d9
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-07-13 21:10:02 +0100

    navbar

--- shill-wtf/src/app/Create-Campaign/page.jsx
+++ shill-wtf/src/app/Create-Campaign/page.jsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function CreateToken() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 
+    bg-gradient-to-b from-white via-pink-200 to-pink-300 ">
       <div className="flex gap-10 h-[70vh]">
         <div className="border w-[40vw] justify-center rounded-3xl">
 
--- shill-wtf/src/app/components/nav/NavBar.jsx
+++ shill-wtf/src/app/components/nav/NavBar.jsx
@@ -1,10 +1,45 @@
+"use client"
+
 import React from "react";
 import WagmiButton from "../buttons/WagmiButton"
+import { useState } from "react";
+
+
 const NavBar = () => {
+
+  const [activeButton, setActiveButton] = useState('REGISTER SHILLER');
+
+
+
   return (
-    <div className="flex justify-between pt-4 px-10" style={{width: '100vw'}}>
+    <div className="flex justify-between pt-4 px-10 bg-white" style={{width: '100vw'}}>
       <p className="text-4xl">SHILL . WTF</p>
-      <button class="bg-sky-500 px-2 pt-2 rounded-lg text-4xl">CONNECT WALLET</button>
+
+
+      <div className="flex items-center justify-center space-x-8">
+      <button
+        className={`handwriting text-xl ${
+          activeButton === 'CAMPAIGN MEME' ? 'text-black' : 'text-gray-300'
+        }`}
+        onClick={() => setActiveButton('CAMPAIGN MEME')}
+      >
+        CAMPAIGN MEME
+      </button>
+      <button
+        className={`handwriting text-xl ${
+          activeButton === 'REGISTER SHILLER' ? 'text-black' : 'text-gray-300'
+        }`}
+        onClick={() => setActiveButton('REGISTER SHILLER')}
+      >
+        REGISTER SHILLER
+      </button>
+    </div>
+
+
+      <button class="bg-yellow-300 hover:bg-yellow-400
+       text-black py-2 px-6 rounded-full border border-black handwriting text-4xl">
+        CONNECT WALLET
+    </button>
     </div>
   );
 };

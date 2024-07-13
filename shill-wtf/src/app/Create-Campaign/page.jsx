45d5283065146c82544dfc5c36f2f00f58ef25d9
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-07-13 21:10:40 +0100

    border black

--- shill-wtf/src/app/Create-Campaign/page.jsx
+++ shill-wtf/src/app/Create-Campaign/page.jsx
@@ -5,11 +5,11 @@ export default function CreateToken() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24 
     bg-gradient-to-b from-white via-pink-200 to-pink-300 ">
       <div className="flex gap-10 h-[70vh]">
-        <div className="border w-[40vw] justify-center rounded-3xl">
+        <div className="border border-black w-[40vw] justify-center rounded-3xl">
 
 
         </div>
-        <div className="border w-[40vw] rounded-3xl">Section 2</div>
+        <div className="border border-black w-[40vw] rounded-3xl">Section 2</div>
       </div>
     </main>
   );

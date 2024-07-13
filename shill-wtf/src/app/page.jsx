f828dd66a28933cffd1b804738f70174389e191e
Kiearn Williams <kieranwilliams@vindico.net>
2024-07-13 21:53:47 +0200

    chgs

--- shill-wtf/src/app/page.jsx
+++ shill-wtf/src/app/page.jsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import NavBar from "./components/nav/NavBar.jsx"
+// import NavBar from "./components/nav/NavBar.jsx"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <NavBar/>
-      <h1 className="text-3xl">Home</h1>
+      <h1 className="text-5xl">HOME</h1>
     </main>
   );
 }

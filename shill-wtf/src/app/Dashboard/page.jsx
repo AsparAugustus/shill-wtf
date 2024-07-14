ae553381829d147fd6815618727caecc4d97c8ac
Kiearn Williams <kieranwilliams@vindico.net>
2024-07-14 07:12:14 +0200

    skew

--- shill-wtf/src/app/Dashboard/page.jsx
+++ shill-wtf/src/app/Dashboard/page.jsx
@@ -1,25 +1,33 @@
 "use client";
 import Image from "next/image";
 import WagmiButton from "../components/buttons/WagmiButton";
+import {useState, useEffect} from "react"
 
 export default function Dashboard() {
+
+  const [tableData, setTableData] = useState([])
+
+  useEffect(() => {
+    setI
+  }, [])
+    
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24
     bg-gradient-to-b from-white via-pink-200 to-pink-300
       ">
       <h1 className="text-3xl mb-8 uppercase">Dashboard</h1>
       <div className="w-full px-4">
-        <div className="overflow-x-auto border rounded-xl">
+        <div className="overflow-x-auto border rounded-xl" style={{transform:"skewX(-8deg)", border: "1px solid #444"}}>
           <table className="min-w-full bg-white border rounded-lg">
             <thead>
               <tr className="w-full bg-gray-200">
-                <th className="py-2 px-4 border-b text-center uppercase">
+                <th className="py-2 px-4 border-b text-center text-2xl uppercase">
                   Token
                 </th>
-                <th className="py-2 px-4 border-b text-center uppercase">
+                <th className="py-2 px-4 border-b text-center text-2xl uppercase">
                   Amount
                 </th>
-                <th className="py-2 px-4 border-b text-center uppercase">
+                <th className="py-2 px-4 border-b text-center text-2xl uppercase">
                   Claim
                 </th>
               </tr>
@@ -56,7 +64,6 @@ export default function Dashboard() {
           </table>
         </div>
       </div>
-      <WagmiButton />
     </main>
   );
 }

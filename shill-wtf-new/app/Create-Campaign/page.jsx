441bbbf25b62b5fb1a05cc6bea9cb823da98da82
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-07-14 03:23:31 +0200

    uoiu

---
+++ shill-wtf-new/app/Create-Campaign/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function CreateToken() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl">Create Campaign</h1>
+    </main>
+  );
+}
---
+++ shill-wtf-new/app/Dashboard/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl">Dashboard</h1>
+    </main>
+  );
+}
